fa31c423bfc4350241b1df2225ebc9e12155cb57
cmtaylor15 <chels.taylor15@gmail.com>
2024-03-11 12:31:29 +0000

    Updated to show London when page loads

--- src/index.js
+++ src/index.js
@@ -21,3 +21,5 @@ function searchSubmit(event) {
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
+
+searchCity("London");
