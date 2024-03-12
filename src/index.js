bf5788621ad416e9ee00deed1f66ac7e416479b3
cmtaylor15 <chels.taylor15@gmail.com>
2024-03-12 15:17:19 +0000

    Updated js to get correct day

--- src/index.js
+++ src/index.js
@@ -79,7 +79,7 @@ function displayForecast(response) {
   let forecastHtml = "";
 
   response.data.daily.forEach(function (day, index) {
-    if (index < 6) {
+    if (index > 0 && index <= 5) {
       forecastHtml =
         forecastHtml +
         `<div class="forecast-day">
